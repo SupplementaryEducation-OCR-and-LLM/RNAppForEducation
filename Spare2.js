83b78b94b6716744de23a94a89d35a5592e63264
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2024-01-07 11:21:26 +0330

    feat: Add initial setup for scanner

--- Spare2.js
+++ Spare2.js
@@ -1,14 +1,47 @@
 import { StyleSheet, Text, View } from "react-native";
 
+// const Spare2 = () => {
+//   return <Text>Open up App.jskdfn to start working on your app!</Text>;
+// };
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+import React, { useState, useEffect } from "react";
+import { Image } from "react-native";
+import DocumentScanner from "react-native-document-scanner-plugin";
+
 const Spare2 = () => {
-  return <Text>Open up App.jskdfn to start working on your app!</Text>;
+  const [scannedImage, setScannedImage] = useState();
+
+  const scanDocument = async () => {
+    // start the document scanner
+    const { scannedImages } = await DocumentScanner.scanDocument();
+
+    // get back an array with scanned image file paths
+    if (scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0]);
+    }
+  };
+
+  useEffect(() => {
+    // call scanDocument on load
+    scanDocument();
+  }, []);
+
+  return (
+    <Image
+      resizeMode="contain"
+      style={{ width: "100%", height: "100%" }}
+      source={{ uri: scannedImage }}
+    />
+  );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
 export default Spare2;
